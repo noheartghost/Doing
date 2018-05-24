af337cd17f69a3351068c252c346efff2eafe25e
zhongchenxing <chenxingzhong1125@gmail.com>
2018-05-24 16:21:47 +0800

    精选页-场馆、课程和教练按钮的点击跳转

--- app/src/main/java/app/doing/com/doing/customView/ImageButtonCustom.java
+++ app/src/main/java/app/doing/com/doing/customView/ImageButtonCustom.java
@@ -20,29 +20,23 @@ import app.doing.com.doing.R;
  * Created by cherry on 18-5-22.
  */
 
-public class ImageButtonCustom extends FrameLayout implements View.OnClickListener{
+public class ImageButtonCustom extends FrameLayout {
     private ImageView imageView;
     private TextView textView;
     private int index;
 
     public ImageButtonCustom(@NonNull Context context, AttributeSet attrs) {
         super(context, attrs);
-        LayoutInflater.from(context).inflate(R.layout.imagebutton_custom,this);
+        LayoutInflater.from(context).inflate(R.layout.imagebutton_custom, this);
         imageView = findViewById(R.id.image_id);
         textView = findViewById(R.id.text_id);
-        imageView.setOnClickListener(this);
-        textView.setOnClickListener(this);
+        //imageView.setOnClickListener(this);
+        //textView.setOnClickListener(this);
 
-        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.ImageButtonCustom);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ImageButtonCustom);
         setText(typedArray.getString(R.styleable.ImageButtonCustom_text));
-        setImage(typedArray.getResourceId(R.styleable.ImageButtonCustom_image,-1));
-        index = typedArray.getInteger(R.styleable.ImageButtonCustom_index,0);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        Log.i("ImageButton",""+index);
+        setImage(typedArray.getResourceId(R.styleable.ImageButtonCustom_image, -1));
+        index = typedArray.getInteger(R.styleable.ImageButtonCustom_index, 0);
 
     }
 
@@ -54,4 +48,8 @@ public class ImageButtonCustom extends FrameLayout implements View.OnClickListen
         imageView.setBackgroundResource(sourceId);
     }
 
+
+    public void onClick(View v) {
+        Log.i("ImageButton","clicked");
+    }
 }
--- app/src/main/java/app/doing/com/doing/handpick/HandpickFragment.java
+++ app/src/main/java/app/doing/com/doing/handpick/HandpickFragment.java
@@ -1,26 +1,66 @@
 package app.doing.com.doing.handpick;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import app.doing.com.doing.R;
+import app.doing.com.doing.customView.ImageButtonCustom;
 
 
 /**
  * Created by cherry on 18-5-21.
  */
 
-public class HandpickFragment extends Fragment {
+public class HandpickFragment extends Fragment implements View.OnClickListener{
+    private ImageButtonCustom gym_ibc;
+    private ImageButtonCustom course_ibc;
+    private ImageButtonCustom coach_ibc;
 
 
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle savedInstanceState){
         View handpickFragment = layoutInflater.inflate(R.layout.handpick_fragment,container,false);
+        initViews(handpickFragment);
 
         return handpickFragment;
     }
 
+    private void initViews(View view){
+        gym_ibc = view.findViewById(R.id.gym_image);
+        course_ibc = view.findViewById(R.id.course_image);
+        coach_ibc = view.findViewById(R.id.coach_image);
 
+        gym_ibc.setOnClickListener(this);
+        course_ibc.setOnClickListener(this);
+        coach_ibc.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        //判断点击的按钮
+        Intent intent;
+        switch (v.getId()){
+            case R.id.gym_image:
+                intent = new Intent(getActivity(),GymListActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.course_image:
+                intent = new Intent(getActivity(),CourseListActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.coach_image:
+                intent = new Intent(getActivity(),CoachListActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
+    }
 }
